4621891c0472c7ffdfbb25757f6ef57aa71b7455
donoprienko <Dmitriy.Onoprienko@vml.com>
2018-04-11 17:59:09 +0300

    routes

---
+++ qr-scan/src/firebase/firebase.js
@@ -0,0 +1,20 @@
+import * as firebase from 'firebase';
+
+const config = {
+  apiKey: "AIzaSyBLucQgujD1w-PfhtccRjUmOopIYiGnL0A",
+  authDomain: "qr-scaner-39e8a.firebaseapp.com",
+  databaseURL: "https://qr-scaner-39e8a.firebaseio.com",
+  projectId: "qr-scaner-39e8a",
+  storageBucket: "qr-scaner-39e8a.appspot.com",
+  messagingSenderId: "929637849133"
+};
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(config);
+}
+
+const auth = firebase.auth();
+
+export {
+  auth,
+};

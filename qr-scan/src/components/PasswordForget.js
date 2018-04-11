ec786197149e928cca27a22a92b31cff673c4cbf
donoprienko <Dmitriy.Onoprienko@vml.com>
2018-04-11 17:41:58 +0300

    routes

---
+++ qr-scan/src/components/PasswordForget.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class PasswordForgetPage extends Component {
+  render() {
+    return (
+      <div>
+        PasswordForgetPage
+      </div>
+    );
+  }
+}
+
+export default PasswordForgetPage;
---
+++ qr-scan/src/components/SignIn.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class SignInPage extends Component {
+  render() {
+    return (
+      <div>
+        SignInPage
+      </div>
+    );
+  }
+}
+
+export default SignInPage;
